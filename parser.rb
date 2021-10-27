617fba176c3067e32757817b1e3015cb41b8752d
Vseotets <metalworldowner@gmail.com>
2021-10-27 18:40:20 +0300

    app logic now works. Parser classes now is separated file

---
+++ parser.rb
@@ -0,0 +1,57 @@
+class BaseParser
+  attr_reader :numbers, :summ , :avrg
+
+  def initialize
+    @numbers = []
+  end
+
+  def check (data)
+    #@numbers.each do |data|
+      if valid?(data)
+        return data
+      else
+        raise "File incorrect!"
+      end
+    #end
+  end
+
+  #проверить на валидность. ексепшн
+  
+  def valid? (num)
+    condition = /^\d*$/
+    return condition.match?(num)
+  end
+    
+  #посчитать сумму и среднее 
+  def sum(array)
+    @summ = 0
+    array.each {|x| @summ += x.to_i }
+    return @summ
+  end
+
+  def calculations
+    @avrg = sum(@numbers).to_f / @numbers.length
+  end
+
+end
+
+class TxtParser < BaseParser
+
+  #прочитать строки
+  def get_numbers
+    File.open(ARGV[0]).read.each_line do |line|
+      @numbers << check(line.strip)
+    end
+  end
+end
+
+class CsvParser < BaseParser
+  
+  #отделить числа от дат
+  def get_numbers
+    File.open(ARGV[0]).read.each_line do |line|
+        @numbers << check(line.split(',')[1].strip)
+    end
+    @numbers.shift
+  end
+end
