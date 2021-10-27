34a249defbd340f2f215d037933343a6df1bfb3f
Vseotets <metalworldowner@gmail.com>
2021-10-27 14:16:23 +0300

    add a headers in csv generator

--- csv_generator.rb
+++ csv_generator.rb
@@ -1,5 +1,6 @@
 file =File.new "numbers.csv","w"
 
 date = "#{rand(1..31)}/#{rand(1..12)}/#{rand(0..9)}#{rand(0..9)}"
+file.puts 'Date, number'
 rand(10..20).times {file.puts "#{date},#{rand(100..999)}"}
 file.close
