6c624820c112e162eeeac02aa5ee21f24fa86c56
Vseotets <metalworldowner@gmail.com>
2021-10-28 11:01:44 +0300

    exeptionsinterpretation when getting file

--- app.rb
+++ app.rb
@@ -1,27 +1,38 @@
 require './parser.rb'
 
   #определить тип документа и запустить соответствующий подкласс
+def start
 puts 'Введите имя файла (.txt/.csv):'
 file = gets.chomp  
-if file.match?(/.csv$/)
-  a = CsvParser.new (file)
-  p 'csv'
-elsif file.match?(/.txt$/)  
-  a = TxtParser.new (file)
-  p 'txt'
-else
-  raise "WRONG TYPE OF FILE!"
+begin
+  if file.match?(/.csv$/)
+    @a = CsvParser.new (file)
+  elsif file.match?(/.txt$/)  
+    @a = TxtParser.new (file)
+  else
+    raise "Wrong type of file!  Use .txt or .csv file"
+  end
+rescue => exeption
+  puts "#{exeption.message}. retry? (y/n)"
+  answer = gets.chomp.capitalize
+  case answer
+    when 'Y'
+      start
+    when 'N'
+      print 'OK, bye!'
+      exit
+    else
+      raise "I can't understand your answer" #понять как вызвать рескью внутри рескью
+    end
 end
-
-
-
-
+end
+start
 #отдельный класс
-a.get_numbers
-a.calculations
+@a.get_numbers
+@a.calculations
 puts "Фаил #{file}"
-puts "Сумма всех чисел: #{a.summ}"
-puts "Среднее значение: #{a.avrg}"
+puts "Сумма всех чисел: #{@a.summ}"
+puts "Среднее значение: #{@a.avrg}"
 
 
 
